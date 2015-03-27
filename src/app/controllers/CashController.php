3d5f50edaff6f3b082f4ac0c815c157c0ca03b8d
Eddie Paredes <eddieparedes11@gmail.com>
2015-03-27 14:45:21 -0600

    Cambio de metodo a modelo de corte de caja

--- src/app/controllers/CashController.php
+++ src/app/controllers/CashController.php
@@ -4,55 +4,13 @@ class CashController extends MvcController{
 
     function cashAdminAction(){
         $model = new ClientHasMovie();
-        $date = Date("Y-m-d");
-        $registers = $model->findAll("DATE_FORMAT(date, '%Y-%m-%d') = '".$date."'");
-        $arrayIdMovies = array();
-        $arrayMovies = array();
-
-        foreach($registers as $register){
-            if(!in_array($register["movie_id"],$arrayIdMovies)){
-                array_push($arrayIdMovies,$register["movie_id"]);
-            }
-        }
-
-        foreach($arrayIdMovies as $id){
-            $cquery = "SELECT SUM(client_has_movie.rented_units) AS total FROM client_has_movie WHERE movie_id=".$id." AND DATE_FORMAT(date, '%Y-%m-%d') = '".$date."'";
-            $result = $model->executeQuery($cquery);
-
-            $movie = new Movie();
-            $movie->finOneById($id);
-            foreach($result as $row){
-                array_push($arrayMovies,array("units"=>$row["total"],"movie"=>$movie));
-            }
-        }
-
+        $arrayMovies = $model->findAllMovieToday();
         $this->render("index",array('arrayMovies' => $arrayMovies), "HeaderAdmin", "admin");
     }
 
     function cashEmployeeAction(){
         $model = new ClientHasMovie();
-        $date = Date("Y-m-d");
-        $registers = $model->findAll("DATE_FORMAT(date, '%Y-%m-%d') = '".$date."'");
-        $arrayIdMovies = array();
-        $arrayMovies = array();
-
-        foreach($registers as $register){
-            if(!in_array($register["movie_id"],$arrayIdMovies)){
-                array_push($arrayIdMovies,$register["movie_id"]);
-            }
-        }
-
-        foreach($arrayIdMovies as $id){
-            $cquery = "SELECT SUM(client_has_movie.rented_units) AS total FROM client_has_movie WHERE movie_id=".$id." AND DATE_FORMAT(date, '%Y-%m-%d') = '".$date."'";
-            $result = $model->executeQuery($cquery);
-
-            $movie = new Movie();
-            $movie->finOneById($id);
-            foreach($result as $row){
-                array_push($arrayMovies,array("units"=>$row["total"],"movie"=>$movie));
-            }
-        }
-
+        $arrayMovies = $model->findAllMovieToday();
         $this->render("index",array('arrayMovies' => $arrayMovies), "HeaderEmployee", "employee");
     }
 } 
--- src/app/models/ClientHasMovie.php
+++ src/app/models/ClientHasMovie.php
@@ -35,21 +35,28 @@ class ClientHasMovie extends ActiveRecord{
 
 	public function findAllMovieToday(){
         $date = Date("Y-m-d");
+        $registers = $this->findAll("DATE_FORMAT(date, '%Y-%m-%d') = '".$date."'");
         $arrayIdMovies = array();
-        $rows = $this->findAll("DATE_FORMAT(date, '%Y-%m-%d') = '".$date."'");
-        print_r($rows);
-        exit();
-        foreach ($rows as $row){
-            if(!in_array($row["movie_id"],$arrayIdMovies)){
-                array_push($arrayIdMovies,$row["movie_id"]);
+        $arrayMovies = array();
+
+        foreach($registers as $register){
+            if(!in_array($register["movie_id"],$arrayIdMovies)){
+                array_push($arrayIdMovies,$register["movie_id"]);
             }
-            /*$model = new Movie();
-            $model->finOneById($row["movie_id"]);
-            array_push($array,array("units"=>$adatos["total"],"movie"=>$controller->findOne($idMovie)));*/
         }
-        print_r($arrayIdMovies);
-        exit();
-        return $arrayIdMovies;
+
+        foreach($arrayIdMovies as $id){
+            $cquery = "SELECT SUM(client_has_movie.rented_units) AS total FROM client_has_movie WHERE movie_id=".$id." AND DATE_FORMAT(date, '%Y-%m-%d') = '".$date."'";
+            $result = $this->executeQuery($cquery);
+
+            $movie = new Movie();
+            $movie->finOneById($id);
+            foreach($result as $row){
+                array_push($arrayMovies,array("units"=>$row["total"],"movie"=>$movie));
+            }
+        }
+
+        return $arrayMovies;
     }
     
     public function findAllMovieRented(){
